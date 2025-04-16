e1a17b2ac16f1c052e165cb52d7104ebde980db5
Mariia <myshyck@gmail.com>
2025-04-16 15:38:30 +0300

    add style notfoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,25 @@
+import style from "./NotFoundPage.module.css";
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
-  )
-}
+    <div className={style.wrapper}>
+      <div className={style.card}>
+        <img
+          src="https://i.imgur.com/A040Lxr.png"
+          alt="Space"
+          className={style.img}
+        />
+        <h3 className={style.title}>This Page is Lost in Space</h3>
+        <p className={style.text}>
+          You thought this mission to the moon would be a quick six month thing.
+          Your neighbor offered to look after your dog. Your high school math
+          teacher was impressed. He once said you wouldn’t amount to
+          anything.You sure showed him. But now here you are, fifty feet from
+          your spaceship with no way to get back. Your dog will be so sad. Your
+          math teacher will be so smug. Pretty devastating.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
