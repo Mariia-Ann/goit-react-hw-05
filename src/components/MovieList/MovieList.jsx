0709583b87cd4a229b5a302bfee135839658b0ce
Mariia <myshyck@gmail.com>
2025-04-16 18:39:21 +0300

    add img for homePage

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,32 @@
 import { Link, useLocation } from "react-router-dom";
-import style from './MovieList.module.css'
+import style from "./MovieList.module.css";
+
+const defaultImg =
+  "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
 
 const MovieList = ({ movies }) => {
   const location = useLocation();
   return (
     <ul className={style.list}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link className={style.item} to={`/movies/${movie.id}`} state={{ from: location }}>
-            {movie.title || movie.name}</Link>
+        <li key={movie.id} className={style.item}>
+          <Link
+            className={style.itemLink}
+            to={`/movies/${movie.id}`}
+            state={{ from: location }}
+          >
+            <img
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                  : defaultImg
+              }
+              width={250}
+              height={350}
+              alt={movie.title}
+            />
+            <p className={style.name}>{movie.title}</p>
+          </Link>
         </li>
       ))}
     </ul>
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -5,6 +5,10 @@ import { searchMovies } from "../../api";
 import style from "./MoviesPage.module.css";
 import Loader from "../../components/Loader/Loader";
 
+const defaultImg =
+  "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
+
+
 const MoviesPage = () => {
   const [movies, setMovies] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
@@ -47,13 +51,23 @@ const MoviesPage = () => {
       {movies.length > 0 && (
         <ul className={style.list}>
           {movies.map((movie) => (
-            <li key={movie.id}>
+            <li key={movie.id} className={style.item}>
               <Link
-                className={style.item}
+                className={style.itemLink}
                 to={`/movies/${movie.id}`}
                 state={{ from: `/movies?query=${movieName}` }}
               >
-                {movie.title || movie.name}
+                <img
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                  : defaultImg
+              }
+              width={250}
+              height={350}
+              alt={movie.title}
+            />
+                <p className={style.name}>{movie.title || movie.name}</p>
               </Link>
             </li>
           ))}
